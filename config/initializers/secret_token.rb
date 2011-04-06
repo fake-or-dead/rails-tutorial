65deae58f03c6bcd20b493b410bdf4f2ea570c5a
fake-or-dead <a.postzone@gmail.com>
2011-04-07 02:00:19 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '7b0a49b07458aef65e5bf9cc2663ef75ea909b6bef5a6fb361d4fda9c84e987f8ef8707c860b01a784e70b6b8d2b00d08bfc08a72b69808740a16db2fd3cebc1'
